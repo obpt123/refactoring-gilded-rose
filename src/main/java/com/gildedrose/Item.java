d1b3027e044a8877c7844cb7132984b01bb749ac
ypb <obpt@sina.com>
2020-03-01 17:43:18 +0800

    [Feature Envy] GildedRole中update_quality方法中都过多依赖Item的行为

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -18,4 +18,55 @@ public class Item {
     public String toString() {
         return this.name + ", " + this.sell_in + ", " + this.quality;
     }
+
+    public void updateItemQuality() {
+        if (!this.name.equals("Aged Brie")
+                && !this.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (this.quality > 0) {
+                if (!this.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    this.quality = this.quality - 1;
+                }
+            }
+        } else {
+            if (this.quality < 50) {
+                this.quality = this.quality + 1;
+
+                if (this.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (this.sell_in < 11) {
+                        if (this.quality < 50) {
+                            this.quality = this.quality + 1;
+                        }
+                    }
+
+                    if (this.sell_in < 6) {
+                        if (this.quality < 50) {
+                            this.quality = this.quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!this.name.equals("Sulfuras, Hand of Ragnaros")) {
+            this.sell_in = this.sell_in - 1;
+        }
+
+        if (this.sell_in < 0) {
+            if (!this.name.equals("Aged Brie")) {
+                if (!this.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (this.quality > 0) {
+                        if (!this.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            this.quality = this.quality - 1;
+                        }
+                    }
+                } else {
+                    this.quality = this.quality - this.quality;
+                }
+            } else {
+                if (this.quality < 50) {
+                    this.quality = this.quality + 1;
+                }
+            }
+        }
+    }
 }
