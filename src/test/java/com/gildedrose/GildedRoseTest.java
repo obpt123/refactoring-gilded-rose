1e55311923aa13905c597686d7b75cc89eb696f9
ypb <obpt@sina.com>
2020-03-01 22:44:06 +0800

    add unit test to cover all logic

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,7 +1,5 @@
 package com.gildedrose;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
 
 import org.junit.Test;
 import static org.mockito.Mockito.*;
--- src/test/java/com/gildedrose/GoodsTest.java
+++ src/test/java/com/gildedrose/GoodsTest.java
@@ -1,6 +1,5 @@
 package com.gildedrose;
 
-import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
@@ -14,6 +13,9 @@ import org.junit.runners.Parameterized.Parameters;
 @RunWith(value = Parameterized.class)
 public class GoodsTest {
     private final static String FOO_NAME = "foo";
+    private final static String AGEDBRIE_NAME = "Aged Brie";
+    private final static String BACKSTAGE_NAME = "Backstage passes to a TAFKAL80ETC concert";
+    private final static String SULFURAS_NAME = "Sulfuras, Hand of Ragnaros";
 
     private final String inputName;
 
@@ -29,6 +31,86 @@ public class GoodsTest {
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][] { 
             { FOO_NAME, 1, 5, 0, 4 }, 
+            { FOO_NAME, 0, 0, -1, 0 }, 
+            { FOO_NAME, 20, 51, 19, 50 }, 
+            { FOO_NAME, 20, 50, 19, 49 }, 
+            { FOO_NAME, 20, 49, 19, 48 }, 
+            { AGEDBRIE_NAME, 20, 52, 19, 52 }, 
+            { AGEDBRIE_NAME, 20, 51, 19, 51 }, 
+            { AGEDBRIE_NAME, 20, 50, 19, 50 }, 
+            { AGEDBRIE_NAME, 20, 49, 19, 50 }, 
+            { AGEDBRIE_NAME, 20, 48, 19, 49 }, 
+            { AGEDBRIE_NAME, 20, 47, 19, 48 }, 
+            { AGEDBRIE_NAME, 1, 52, 0, 52 }, 
+            { AGEDBRIE_NAME, 1, 51, 0, 51 }, 
+            { AGEDBRIE_NAME, 1, 50, 0, 50 }, 
+            { AGEDBRIE_NAME, 1, 49, 0, 50 }, 
+            { AGEDBRIE_NAME, 1, 48, 0, 49 }, 
+            { AGEDBRIE_NAME, 1, 47, 0, 48 }, 
+            { AGEDBRIE_NAME, 0, 52, -1, 52 }, 
+            { AGEDBRIE_NAME, 0, 51, -1, 51 }, 
+            { AGEDBRIE_NAME, 0, 50, -1, 50 }, 
+            { AGEDBRIE_NAME, 0, 49, -1, 50 }, 
+            { AGEDBRIE_NAME, 0, 48, -1, 50 }, 
+            { AGEDBRIE_NAME, 0, 47, -1, 49 }, 
+            { AGEDBRIE_NAME, 0, 46, -1, 48 },
+            { BACKSTAGE_NAME, 20, 52, 19, 52 }, 
+            { BACKSTAGE_NAME, 20, 51, 19, 51 }, 
+            { BACKSTAGE_NAME, 20, 50, 19, 50 }, 
+            { BACKSTAGE_NAME, 20, 49, 19, 50 }, 
+            { BACKSTAGE_NAME, 20, 48, 19, 49 }, 
+            { BACKSTAGE_NAME, 20, 47, 19, 48 }, 
+            { BACKSTAGE_NAME, 20, 46, 19, 47 },
+
+            { BACKSTAGE_NAME, 10, 52, 9, 52 }, 
+            { BACKSTAGE_NAME, 10, 51, 9, 51 }, 
+            { BACKSTAGE_NAME, 10, 50, 9, 50 }, 
+            { BACKSTAGE_NAME, 10, 49, 9, 50 }, 
+            { BACKSTAGE_NAME, 10, 48, 9, 50 }, 
+            { BACKSTAGE_NAME, 10, 47, 9, 49 },
+            { BACKSTAGE_NAME, 10, 46, 9, 48 }, 
+
+            { BACKSTAGE_NAME, 5, 52, 4, 52 }, 
+            { BACKSTAGE_NAME, 5, 51, 4, 51 }, 
+            { BACKSTAGE_NAME, 5, 50, 4, 50 }, 
+            { BACKSTAGE_NAME, 5, 49, 4, 50 }, 
+            { BACKSTAGE_NAME, 5, 48, 4, 50 }, 
+            { BACKSTAGE_NAME, 5, 47, 4, 50 },
+            { BACKSTAGE_NAME, 5, 46, 4, 49 }, 
+
+            { BACKSTAGE_NAME, 1, 52, 0, 52 }, 
+            { BACKSTAGE_NAME, 1, 51, 0, 51 }, 
+            { BACKSTAGE_NAME, 1, 50, 0, 50 }, 
+            { BACKSTAGE_NAME, 1, 49, 0, 50 }, 
+            { BACKSTAGE_NAME, 1, 48, 0, 50 }, 
+            { BACKSTAGE_NAME, 1, 47, 0, 50 }, 
+            { BACKSTAGE_NAME, 1, 46, 0, 49 }, 
+            { BACKSTAGE_NAME, 0, 52, -1, 0 }, 
+            { BACKSTAGE_NAME, 0, 51, -1, 0 }, 
+            { BACKSTAGE_NAME, 0, 50, -1, 0 }, 
+            { BACKSTAGE_NAME, 0, 49, -1, 0 }, 
+            { BACKSTAGE_NAME, 0, 48, -1, 0 }, 
+            { BACKSTAGE_NAME, 0, 47, -1, 0 }, 
+            { BACKSTAGE_NAME, 0, 46, -1, 0 },
+
+            { SULFURAS_NAME, 20, 52, 20, 52 }, 
+            { SULFURAS_NAME, 20, 51, 20, 51 }, 
+            { SULFURAS_NAME, 20, 50, 20, 50 }, 
+            { SULFURAS_NAME, 20, 49, 20, 49 }, 
+            { SULFURAS_NAME, 20, 48, 20, 48 }, 
+            { SULFURAS_NAME, 20, 47, 20, 47 },
+            { SULFURAS_NAME, 1, 52, 1, 52 }, 
+            { SULFURAS_NAME, 1, 51, 1, 51 }, 
+            { SULFURAS_NAME, 1, 50, 1, 50 }, 
+            { SULFURAS_NAME, 1, 49, 1, 49 }, 
+            { SULFURAS_NAME, 1, 48, 1, 48 }, 
+            { SULFURAS_NAME, 1, 47, 1, 47 }, 
+            { SULFURAS_NAME, 0, 52, 0, 52 }, 
+            { SULFURAS_NAME, 0, 51, 0, 51 }, 
+            { SULFURAS_NAME, 0, 50, 0, 50 }, 
+            { SULFURAS_NAME, 0, 49, 0, 49 }, 
+            { SULFURAS_NAME, 0, 48, 0, 48 }, 
+            { SULFURAS_NAME, 0, 47, 0, 47 }, 
         });
     }
 
@@ -44,9 +126,9 @@ public class GoodsTest {
     public void shouldGetExpectedValueWhenUpdateInfoWithParams() {
         Goods goods = new Goods(this.inputName, this.inputSellIn, this.inputQuality);
         goods.updateInfo();
-        assertEquals(goods.getName(), this.inputName);
-        assertThat(goods.getSellIn(), is(this.expectedSellIn));
-        assertThat(goods.getQuality(), is(this.expectedQuality));
+        assertEquals("Name", this.inputName, goods.getName());
+        assertEquals("SellIn", this.expectedSellIn, goods.getSellIn());
+        assertEquals("Quality", this.expectedQuality, goods.getQuality());
     }
 
 }
